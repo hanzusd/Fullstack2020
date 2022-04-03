cfeed091c01a5c9a319fde81114885c32b57740a
hanzusd <hannalotta.alenius@helsinki.fi>
2022-04-03 22:44:02 +0300

    exercises 4.5-4.7

--- osa4/bloglist_backend/utils/list_helper.js
+++ osa4/bloglist_backend/utils/list_helper.js
@@ -18,8 +18,75 @@ const dummy = (blogs) => {
           return (allLikes)
       }
   }
+
+  const favouriteBlog = (blogs) => {
+      likedBlog = blogs[0]
+      blogs.forEach(blog => {
+          if (blog.likes > likedBlog.likes) {
+              likedBlog = blog
+          }
+      });
+      return(likedBlog)
+  }
+
+  const mostBlogs = (blogs) => {
+    blogsByAuthor = {}
+
+    for (let i = 0; i < blogs.length; i++) {
+        var currentAuthor = blogs[i].author
+        if (blogsByAuthor.length===0) {
+            var toBeAdded = {author: currentAuthor, blogs: 1}
+            blogsByAuthor[currentAuthor] = toBeAdded
+        } else {
+            if(blogsByAuthor[currentAuthor]) {
+                var change = blogsByAuthor[currentAuthor].blogs + 1
+                blogsByAuthor[currentAuthor].blogs = change
+            } else {
+                var addingAuthor = {author: currentAuthor, blogs:1}
+                blogsByAuthor[currentAuthor] = addingAuthor
+            }
+        } 
+    }
+    var authorMostBlogs = blogsByAuthor[blogs[0].author]
+    for (const [name, val] of Object.entries(blogsByAuthor)) {
+        if(val.blogs > authorMostBlogs.blogs) {
+            authorMostBlogs = val
+        }
+    }
+    return(authorMostBlogs)
+}
+
+const mostLikes = (blogs) => {
+    blogsByLikes = {}
+
+    for (let i = 0; i < blogs.length; i++) {
+        var currentAuthor = blogs[i].author
+        if (blogsByLikes.length===0) {
+            const toBeAdded = {author: currentAuthor, likes: blogs[i].likes}
+            blogsByLikes[currentAuthor] = toBeAdded
+        } else {
+            if(blogsByLikes[currentAuthor]) {
+                var change = blogsByLikes[currentAuthor].likes + blogs[i].likes
+                blogsByLikes[currentAuthor].likes = change
+            } else {
+                var addingAuthor = {author: currentAuthor, likes: blogs[i].likes}
+                blogsByLikes[currentAuthor] = addingAuthor
+            }
+        } 
+    }
+    var authorMostLikes = blogsByLikes[blogs[0].author]
+    for (const [name, val] of Object.entries(blogsByLikes)) {
+        if(val.likes > authorMostLikes.likes) {
+            authorMostLikes = val
+        }
+    }
+    return (authorMostLikes)
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes, 
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
   }
