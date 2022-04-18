a8c51cce252faeae8b4b3a628008df685446b4e3
hanzusd <hannalotta.alenius@helsinki.fi>
2022-04-18 20:56:49 +0300

    exercise 4.10

--- osa4/bloglist_backend/tests/blog_api.test.js
+++ osa4/bloglist_backend/tests/blog_api.test.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "Minun Blogi",
+        author: "MÄ",
+        url: "osoite", 
+        likes: 2
+    },
+    {
+        title: "Sinun Blogi",
+        author: "SÄ",
+        url: "nettiosoite",
+        likes: 3
+    }
+]
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -20,6 +42,28 @@ test('unique identifier property of the blog posts is named id', async () => {
     const response = await api.get('/api/blogs')
     
     expect(response.body[0].id).toBeDefined()
+})
+
+test('POST successfully creates a new blog post', async () => {
+    const newBlog = {
+        title: "Test Blog",
+        author: "Tester",
+        url: "testing url", 
+        likes: 8
+    }
+    
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(b => b.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain('Test Blog')
 })
 
 afterAll(() => {
