212edbad7e2b0f38ec74b58f20984a70eda35de0
hanzusd <hannalotta.alenius@helsinki.fi>
2022-05-07 15:38:32 +0300

    exercise 4.18

--- osa4/bloglist_backend/app.js
+++ osa4/bloglist_backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoose = require('mongoose')
 
@@ -15,5 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
