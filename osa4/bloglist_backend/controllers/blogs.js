1b64b13a76a5be9e65e17ad52d4dac4051bf038c
hanzusd <hannalotta.alenius@helsinki.fi>
2022-05-07 16:46:01 +0300

    exercise 4.19

--- osa4/bloglist_backend/controllers/blogs.js
+++ osa4/bloglist_backend/controllers/blogs.js
@@ -1,24 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username:1, name: 1})
 
     response.json(blogs)
   })
+
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
   
 blogsRouter.post('/', async (request, response) => {
     console.log('request:', request.body)
     const body = request.body
-    const users = await User.find({})
+    
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: 0,
-      user: users[0]._id
+      user: user._id
     })
 
     const savingBlog = await blog.save()
@@ -49,8 +64,16 @@ blogsRouter.post('/', async (request, response) => {
   const errorHandler = (error, request, response, next) => {
     if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
-      }
-      next(error)
+    } else if (error.name === 'JsonWebTokenError') {
+      return response.status(401).json({
+        error: 'invalid token'
+      })
+    } else if (error.name === 'TokenExpiredError') {
+      return response.status(401).json({
+        error: 'token expired'
+      })
+    }
+    next(error)
   }
 
 blogsRouter.use(errorHandler)
