5db769ee6652c496f37d5a96ddf740b68ce7503c
hanzusd <hannalotta.alenius@helsinki.fi>
2022-09-07 20:24:09 +0300

    exercise 9.5

--- osa9/bmi_and_ec/bmiCalculator.ts
+++ osa9/bmi_and_ec/bmiCalculator.ts
@@ -1,27 +1,23 @@
 //bmi is an American scam
 
-const calculateBmi = (a: number, b: number) => {
+export const calculateBmi = (a: number, b: number): string => {
   const bmi = b/(a/100)/(a/100)
   
   if (bmi < 16)  {
-    console.log('Underweight (Severe thinness)')
+    return 'Underweight (Severe thinness)'
   } else if (bmi < 17)  {
-    console.log('Underweight (Moderate thinness)')
+    return 'Underweight (Moderate thinness)'
   } else if (bmi < 18.5)  {
-    console.log('Underweight (Mild thinness)')
+    return 'Underweight (Mild thinness)'
   } else if (bmi < 25)  {
-    console.log('Normal (Healthy weight)')
+    return 'Normal (Healthy weight)'
   } else if (bmi < 30)  {
-    console.log('Overweight (Pre-obese)')
+    return 'Overweight (Pre-obese)'
   } else if (bmi < 35)  {
-    console.log('Obese (Class I)')
+    return 'Obese (Class I)'
   } else if (bmi < 40)  {
-    console.log('Obese (Class II)')
+    return 'Obese (Class II)'
   } else {
-    console.log('Obese (Class III)')
+    return 'Obese (Class III)'
   }
 }
-
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
-calculateBmi(a, b);
--- osa9/bmi_and_ec/index.ts
+++ osa9/bmi_and_ec/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+  const height = parseInt(String(req.query.height))
+  const weight = parseInt(String(req.query.weight))
+  if (isNaN(weight) || isNaN(height)) {
+    res.status(400).json({
+      error: 'malformatted parameters'
+    })
+    return
+  }
+  const bmi = calculateBmi(Number(req.query.height), Number(req.query.weight))
+  res.json({
+    weight,
+    height,
+    bmi
+  })
+})
 
 const PORT = 3003;
 
